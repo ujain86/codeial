e642c6393d809dd1d7239eeb5360c890e57b84b3
Ujjwal <ujjwal.jain116@gmail.com>
2022-08-14 08:57:29 +0530

    Render home page and users/profile page using ejs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,10 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
-}
+    // res.end('<h1>User Profile</h1>');
+    return res.render('users_profile', {
+        title: 'Users/Profile',
+    });
+};
 
 module.exports.name = function(req,res){
     res.end('<h1>User Name</h1>');
-}
+};
