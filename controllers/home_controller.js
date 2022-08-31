f2928e33dfbd9d81a5b21a2e8ee735fd0518d252
Ujjwal <ujjwal.jain116@gmail.com>
2022-08-31 22:15:28 +0530

    Display all posts on home page with name of author

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,33 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // return res.end('<h1>Express server is up</h1>');
-    return res.render('home',{
-        title: 'Home',
+
+    // Post.find({}, function(err, posts){
+    //     if(err){
+    //         console.log('Error in fetching posts from db');
+    //         return;
+    //     }
+
+    //     return res.render('home',{
+    //         title: 'Home',
+    //         posts: posts
+    //     });
+
+    // });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){
+            console.log('Error in fetching posts from db');
+            return;
+        }
+
+        return res.render('home',{
+            title: 'Home',
+            posts: posts
+        });
+    
     });
+    
 };
